1cbbf4253ce25c291dee745b210f6be5ece83740
KostyaZlb <kostazlobin95@gmail.com>
2019-01-25 16:07:11 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+template < typename T > void
+printB (T data)
+{
+  cout << pow (2, sizeof (data)) << endl;
+  for (int i = pow (2, sizeof (data)); i >= 0; --i)
+    {
+      cout << ((data >> i) & 1);
+    }
+  cout << "\n";
+}
+
+int
+main ()
+{
+  int i;
+
+  i = ((unsigned short) 21 & 0xff) << 8;
+  i |= ((unsigned short) 3 & 0x0f) << 4;
+  i |= ((unsigned short) 1 & 0x01) << 3;
+  cout << "byte word: ";
+  printB (i);
+
+  cout << ((i >> 8) & 0xff) << endl;
+  cout << ((i >> 4) & 0x0f) << endl;
+  cout << ((i >> 3) & 0x01) << endl;
+
+  return 0;
+}
